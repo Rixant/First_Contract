56ddb37b74b24731194efc13295ff84634f5e57e
rixant <rixantrokaha@gmail.com>
2022-12-14 23:27:56 -0500

    campaign_contract: Created approveRequest function.

--- campaign-contract/contracts/campaign.sol
+++ campaign-contract/contracts/campaign.sol
@@ -7,12 +7,14 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    mapping(address => bool) public approvers;
 
     modifier restricted() {
         require(msg.sender == manager);
@@ -26,19 +28,30 @@ contract Campaign {
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
     }
 
-    function request(string description, uint value, address recipient) 
+    function createRequest(string description, uint value, address recipient) 
         public restricted {
             Request memory newRequest = Request({
                 description: description,
                 value: value,
                 recipient: recipient,
-                complete: false
+                complete: false,
+                approvalCount: 0
             });
 
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
     }
 
 }
