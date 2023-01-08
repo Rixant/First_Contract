011b47dd608e8fd9fad6837bb3db96ab8651889f
rixant <rixantmgr@gmail.com>
2023-01-08 18:04:13 -0500

    campaign_frontend: Created a table for displaying requests.

--- campaign-frontend/pages/campaigns/requests/index.js
+++ campaign-frontend/pages/campaigns/requests/index.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
-import Campaign from '../../../ethereum/campaign';
 import MyCampaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
-const RequestIndex = ({address, requests}) => {
+const RequestIndex = ({address, requests, requestsCount, approversCount}) => {
+
+    const { Header, Row, HeaderCell, Body } = Table
+
+    const renderRows = () => {
+        return requests.map((request, index) => {
+            console.log(request);
+            return <RequestRow
+                key={index}
+                id={index}
+                request={request}
+                address={address}
+                approversCount={approversCount}
+             />;
+        })
+    }
 
     return (
         <Layout>
             <h3>Requests</h3>
             <Link route={`/campaigns/${address}/requests/new`}>
-                <Button primary>Add Request</Button>
+                <Button primary floated="right" style={{ marginBottom: 10 }}>Add Request</Button>
             </Link>
+
+            <Table>
+                <Header>
+                    <Row>
+                        <HeaderCell>ID</HeaderCell>
+                        <HeaderCell>Description</HeaderCell>
+                        <HeaderCell>Amount</HeaderCell>
+                        <HeaderCell>Recipient</HeaderCell>
+                        <HeaderCell>Approval</HeaderCell>
+                        <HeaderCell>Approve</HeaderCell>
+                        <HeaderCell>Finalize</HeaderCell>
+                    </Row>
+                </Header>
+                <Body>
+                    {renderRows()}
+                </Body>
+            </Table>
+            <div>Found {requestsCount} requests.</div>
         </Layout>
     )
 }
@@ -20,17 +53,18 @@ const RequestIndex = ({address, requests}) => {
 RequestIndex.getInitialProps = async (props) => {
     const {address} = props.query;
     const campaign = MyCampaign(address);
-    const requestCount = await campaign.methods.getRequestsCount().call();
+    const requestsCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
 
     const requests = await Promise.all(
-        Array(parseInt(requestCount))
+        Array(parseInt(requestsCount))
             .fill()
             .map((element, index) => {
                 return campaign.methods.requests(index).call()
             })
     );
 
-    return {address, requests};
+    return {address, requests, requestsCount, approversCount};
 };
 
 export default RequestIndex;
