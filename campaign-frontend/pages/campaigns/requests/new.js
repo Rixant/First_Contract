49dab8c079317e20fc4fa49a2cdd41ebfc4a3977
rixant <rixantmgr@gmail.com>
2023-01-08 16:52:31 -0500

    campaign_frontend: Developed create request logic.

--- campaign-frontend/pages/campaigns/requests/new.js
+++ campaign-frontend/pages/campaigns/requests/new.js
@@ -1,19 +1,50 @@
 import React from 'react';
 import { Form, Button, Message, Input } from 'semantic-ui-react';
-import Campaign from '../../../ethereum/campaign';
+import MyCampaign from '../../../ethereum/campaign';
 import web3 from '../../../ethereum/web3';
 import { Link, Router } from '../../../routes';
 import Layout from '../../../components/Layout';
 
-const RequestNew = ({ adddress }) => {
+const RequestNew = ({ address }) => {
 
-    const [value, setValue] = React.useState(100);
+    const [value, setValue] = React.useState(0);
     const [description, setDescription] = React.useState('Coin');
     const [recipient, setRecipient] = React.useState(null);
+    const [loading, setLoading] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        const campaign = MyCampaign(address);
+
+        setLoading(true);
+        setErrorMessage('');
+
+        // create a new request
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.createRequest(
+                description,
+                web3.utils.toWei(value, 'ether'),
+                recipient
+            ).send({from: accounts[0]})
+
+            Router.pushRoute(`/campaigns/${address}/requests`);
+        } catch (err) {
+            setErrorMessage(err.message);
+        }
+
+        setLoading(false);
+    }
 
     return (
         <Layout>
-            <Form>
+            <Link route={`/campaigns/${address}/requests`}>
+                Back
+            </Link>
+
+            <Form onSubmit={onSubmit} error={!!errorMessage}>
                 <Form.Field>
                     <label>Description</label>
                     <Input 
@@ -40,7 +71,8 @@ const RequestNew = ({ adddress }) => {
                     />
                 </Form.Field>
 
-                <Button primary>Create!</Button>
+                <Message error header="Oops!" content={errorMessage} />
+                <Button primary loading={loading}>Create!</Button>
             </Form>
         </Layout>
     )
